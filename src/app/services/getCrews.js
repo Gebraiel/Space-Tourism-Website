ef17dc1e0e1cd3f76aad7d8311015e8960b913e3
Gebraiel <67439330+Gebraiel@users.noreply.github.com>
2025-05-16 12:55:32 +0300

    edit hostname before uploading

--- src/app/services/getCrews.js
+++ src/app/services/getCrews.js
@@ -1,5 +1,5 @@
 export default async function getCrews(){
-  const res = await fetch('http://localhost:3000/api/crews',{ cache: "no-store" });
+  const res = await fetch('https://space-tourism-website-pi-seven.vercel.app/api/crews',{ cache: "no-store" });
   const crews = await res.json();
   return crews;
 }
--- src/app/services/getDestinations.js
+++ src/app/services/getDestinations.js
@@ -1,5 +1,5 @@
 export default async function getDestinations(){
-  const res = await fetch('http://localhost:3000/api/destinations',{ cache: "no-store" });
+  const res = await fetch('https://space-tourism-website-pi-seven.vercel.app/api/destinations',{ cache: "no-store" });
   const destinations = await res.json();
   return destinations;
 }
